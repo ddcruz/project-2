17db23c1b1ade7eed080c725e9d332cefafd6385
deXta <ddcruz@email.com>
2019-03-12 21:12:43 -0500

    added store app

---
+++ store/models.py
@@ -0,0 +1,13 @@
+from .app import db
+
+
+class Store(db.Model):
+    __tablename__ = 'store'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    lat = db.Column(db.Float)
+    lon = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<Store %r>' % (self.name)
