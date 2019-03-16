e96b1abae9fda34303af98be43052463ce5a8b28
deXta <ddcruz@email.com>
2019-03-16 17:28:59 -0500

    the on Click of each state draws circles per city/town with the population density as the radius of each circle

--- store/models.py
+++ store/models.py
@@ -1,12 +1,16 @@
 from .app import db
 
-class Store(db.Model):
-    __tablename__ = 'store'
+class City(db.Model):
+    __tablename__ = 'city'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64))
+    city = db.Column(db.String(64))
+    state_abbr = db.Column(db.String(2))
+    state = db.Column(db.String(64))
+    county = db.Column(db.String(64))
     lat = db.Column(db.Float)
     lon = db.Column(db.Float)
+    density = db.Column(db.Float)
 
     def __repr__(self):
-        return '<Store %r>' % (self.name)
+        return '<City %r>' % (self.name)
