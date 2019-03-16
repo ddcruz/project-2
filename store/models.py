9a7ed3d1a9d29e298e38400669c186992bd112e7
deXta <ddcruz@email.com>
2019-03-16 18:21:58 -0500

    manually merged issue_6 for models.py

--- store/models.py
+++ store/models.py
@@ -14,3 +14,36 @@ class City(db.Model):
 
     def __repr__(self):
         return '<City %r>' % (self.name)
+
+class City_Demo(db.Model):
+    __tablename__ = 'city_demographics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    city = db.Column(db.String(64))
+    state = db.Column(db.String(64))
+    state_abbr = db.Column(db.String(64))
+    population = db.Column(db.Float)
+    median_age = db.Column(db.Float)
+    average_household_size = db.Column(db.Float)
+    median_income = db.Column(db.Float)
+    household_income = db.Column(db.Float)
+    household_income = db.Column(db.Float)
+
+
+    def __repr__(self):
+        return '<City_Demo %r>' % (self.name)
+
+class State_Demo(db.Model):
+    __tablename__ = 'state_demographics'
+
+    id = db.Column(db.Integer, primary_key=True)
+    state = db.Column(db.String(64))
+    population = db.Column(db.Float)
+    median_age = db.Column(db.Float)
+    average_household_size = db.Column(db.Float)
+    median_income = db.Column(db.Float)
+    household_income = db.Column(db.Float)
+    household_income = db.Column(db.Float)
+
+    def __repr__(self):
+        return '<State_Demo %r>' % (self.name) 
